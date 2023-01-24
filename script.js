882664178035f56108ce685ed260afe3b5a0896b
Jose Szycho <joseszychowski@gmail.com>
2023-01-24 08:53:28 -0300

    Finish app

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+import {API_KEY} from "./API_KEY.js";
+
 class APIManager {
-    static API_KEY = "0e5dea7f846151201d9791b821e256f3";
+    static API_KEY = API_KEY;
 
     static getCallLink(city, units = "metric"){
         const callLink = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.API_KEY}&units=${units}`;
@@ -16,7 +18,51 @@ class APIManager {
 
 }
 
-APIManager.getCurrentWeather("Posadas", "metric")
-    .then(console.log)
-    .catch(hola => console.log("feo"))
+class Modal {
+    static feelsLike;
+    static humidity;
+    static pressure; 
+    static temperature;
+    static city;
+    static country;
 
+    static getCurrentWeather(){
+        event.preventDefault();
+        const city = document.getElementById("cityInput").value;
+        APIManager.getCurrentWeather(city)
+        .then( response => {
+            this.feelsLike = response.main.feels_like;
+            this.humidity = response.main.humidity;
+            this.pressure = response.main.pressure;
+            this.temperature = response.main.temp;
+            this.city = response.name;
+            this.country = response.sys.country;
+            Modal.Show();
+        })
+        .catch(error => alert(error));
+    }
+
+    static Show(){
+        let modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('myModal')) // Returns a Bootstrap modal instance
+        
+        let modalBody = document.getElementById("modalBody"); 
+        modalBody.innerHTML =  `<p>Feels like: ${Math.round(this.feelsLike)}</p>
+                                <p>Humidity: ${Math.round(this.humidity)}</p>
+                                <p>Pressure: ${Math.round(this.pressure)}</p>
+                                <p>Temperature: ${Math.round(this.temperature)}</p>`;
+
+        let modalTtile = document.getElementById("modalTitle");
+        modalTtile.innerHTML = `${this.city}, ${this.country}`;
+        
+        modal.show();
+    }
+}
+
+
+
+
+const submitButton = document.addEventListener("submit", () => Modal.getCurrentWeather())
+
+
+    
+   
